271628ca5433f770f471188f1be35caf60e50a9d
TreetopFlyer <seth111@gmail.com>
2017-04-11 19:49:50 -0400

    bodyparser and cookieparser added

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 var express = require('express');
 var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 
 var server = express();
-server.use("/static", express.static(__dirname+'/static'));
 server.engine('handlebars', handlebars());
 server.set('view engine', 'handlebars');
-
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.user(cookieParser());
+server.use("/static", express.static(__dirname+'/static'));
+server.get("/", function(inReq, inRes)
+{
+    inRes.render("home", {title:"Home page", layout:"main"});
+});
 module.exports = server;
